ade74700108f8fccad23f46697f236817155a373
张岱墀 <zhang_daichi@163.com>
2019-03-27 16:35:23 +0800

    deeeeeeebug

--- labcodes/lab3/kern/mm/swap_fifo.c
+++ labcodes/lab3/kern/mm/swap_fifo.c
@@ -49,13 +49,9 @@ _fifo_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int
  
     assert(entry != NULL && head != NULL);
     //record the page access situlation
-    /*LAB3 EXERCISE 2: 2016011364*/ 
+    /*LAB3 EXERCISE 2: YOUR CODE*/ 
     //(1)link the most recent arrival page at the back of the pra_list_head qeueue.
-    while(list_next(head) != NULL) {
-        head = list_next(head);
-    }
-    list_add_after(head, entry);
-
+    list_add(head, entry);
     return 0;
 }
 /*
@@ -65,17 +61,21 @@ _fifo_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int
 static int
 _fifo_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
 {
-    list_entry_t *head=(list_entry_t*) mm->sm_priv;
-    assert(head != NULL);
-    assert(in_tick==0);
-    /* Select the victim */
-    /*LAB3 EXERCISE 2: 2016011364*/ 
-    //(1)  unlink the earliest arrival page in front of pra_list_head qeueue
-    struct Page *p = le2page(head, pra_page_link);
-    list_del(head);
-    //(2)  assign the value of *ptr_page to the addr of this page
-    * ptr_page = p;
-    return 0;
+     list_entry_t *head=(list_entry_t*) mm->sm_priv;
+         assert(head != NULL);
+     assert(in_tick==0);
+     /* Select the victim */
+     /*LAB3 EXERCISE 2: YOUR CODE*/ 
+     //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
+     //(2)  assign the value of *ptr_page to the addr of this page
+     /* Select the tail */
+     list_entry_t *le = head->prev;
+     assert(head!=le);
+     struct Page *p = le2page(le, pra_page_link);
+     list_del(le);
+     assert(p !=NULL);
+     *ptr_page = p;
+     return 0;
 }
 
 static int
