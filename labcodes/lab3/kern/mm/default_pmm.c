9d3a84f2cf0bf871b9a31602dd1c9127039af610
张岱墀 <zhang_daichi@163.com>
2019-03-27 16:37:45 +0800

    fifo

--- labcodes/lab3/kern/mm/default_pmm.c
+++ labcodes/lab3/kern/mm/default_pmm.c
@@ -12,7 +12,7 @@
  *  Please refer to Page 196~198, Section 8.2 of Yan Wei Min's Chinese book
  * "Data Structure -- C programming language".
 */
-// LAB2 EXERCISE 1: YOUR CODE
+// LAB2 EXERCISE 1: 2016011364
 // you should rewrite functions: `default_init`, `default_init_memmap`,
 // `default_alloc_pages`, `default_free_pages`.
 /*
@@ -105,12 +105,13 @@ default_init(void) {
 }
 
 static void
-default_init_memmap(struct Page *base, size_t n) {
+default_init_memmap(struct Page *base, size_t n) {  // 初始化一块block，相当于在list的每一项下面拉链，第一页存下size
     assert(n > 0);
     struct Page *p = base;
     for (; p != base + n; p ++) {
         assert(PageReserved(p));
-        p->flags = p->property = 0;
+	p -> flags = 0;
+        p -> property = 0;
         set_page_ref(p, 0);
     }
     base->property = n;
@@ -127,7 +128,6 @@ default_alloc_pages(size_t n) {
     }
     struct Page *page = NULL;
     list_entry_t *le = &free_list;
-    // TODO: optimize (next-fit)
     while ((le = list_next(le)) != &free_list) {
         struct Page *p = le2page(le, page_link);
         if (p->property >= n) {
@@ -136,15 +136,15 @@ default_alloc_pages(size_t n) {
         }
     }
     if (page != NULL) {
+        list_del(&(page->page_link));       // 先把找到的该block从list里面删掉
         if (page->property > n) {
             struct Page *p = page + n;
-            p->property = page->property - n;
+            p->property = page->property - n;   // 修改该block的free pages数量
             SetPageProperty(p);
-            list_add_after(&(page->page_link), &(p->page_link));
+            list_add(&free_list, &(p->page_link));  // 将新的block加入到list中
         }
-        list_del(&(page->page_link));
-        nr_free -= n;
-        ClearPageProperty(page);
+        nr_free -= n;                       // 更新总的free pages数
+        ClearPageProperty(page);            // 更新该block对应相关bit位
     }
     return page;
 }
@@ -164,7 +164,6 @@ default_free_pages(struct Page *base, size_t n) {
     while (le != &free_list) {
         p = le2page(le, page_link);
         le = list_next(le);
-        // TODO: optimize
         if (base + base->property == p) {
             base->property += p->property;
             ClearPageProperty(p);
@@ -320,3 +319,4 @@ const struct pmm_manager default_pmm_manager = {
     .nr_free_pages = default_nr_free_pages,
     .check = default_check,
 };
+
